034629b2166c9e20214cbb6d46871c359d8e3f82
deivison resende <deivison.resende@enacom.com.br>
2023-05-17 20:54:26 -0300

    feat: add memories table and routes

---
+++ src/routes/memories.ts
@@ -0,0 +1,76 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+import { z } from 'zod'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const data = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({ data })
+
+    return memory
+  })
+
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memory) => ({
+      id: memory.id,
+      coverUrl: memory.coverUrl,
+      excerpt: memory.content.substring(0, 115).concat('...'),
+    }))
+  })
+
+  app.get('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return memory
+  })
+
+  app.put('/memories/:id', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.update({
+      where: { id },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
+
+    return memory
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
-
+import { memoriesRoutes } from './routes/memories'
 const app = fastify()
 
 app
+  .register(cors, { origin: true })
+  .register(memoriesRoutes)
   .listen({ port: 3333 })
   .then(() => console.log('HTTP server running on http://localhost:3333'))
